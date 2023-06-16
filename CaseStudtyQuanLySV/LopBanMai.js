93269758d070fe6fd74748a6e9b1be8dc7b75f27
Buianhkhoa2510 <trinhthuylinh03121997@gmail.com>
2023-06-15 23:10:54 -0700

    casepart3

---
+++ CaseStudtyQuanLySV/LopBanMai.js
@@ -0,0 +1,17 @@
+class LopBanMai {
+    constructor(student) {
+        this.student = student
+    }
+    getStudent() {
+        return this.student
+    }
+    setStudent() {
+        student = this.student
+    }
+    addStudent() {
+student += this.student
+    }
+    deleteStudent() {
+student -=this.student
+    }
+}
---
+++ CaseStudtyQuanLySV/Student.js
@@ -0,0 +1,63 @@
+class Student{
+    constructor(no, name, dob, male, address, parents, phone, note){
+        this.no = no
+        this.name = name
+        this.dob = dob
+        this.male = male
+        this.address = address
+        this.parents = parents
+        this.phone = phone
+        this.note = note
+    }
+    getNo(){
+
+    }
+    setNo(){
+
+    }
+    getName(){
+
+    }
+    setName(){
+
+    }
+    getDob(){
+
+    }
+    setDob(){
+
+    }
+    getMale(){
+
+    }
+    setMale(){
+
+    }
+    getAddress(){
+
+    }
+    setAddress(){
+
+    }
+    getParents(){
+
+    }
+    setParents(){
+
+    }
+    getPhone(){
+
+    }
+    setPhone(){
+
+    }
+    getNote(){
+
+    }
+    setNote(){
+
+    }
+    editStudent(){
+        
+    }
+}
