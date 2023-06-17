4d9cb895eacf51d5a87c4ce32b27d62221830752
Buianhkhoa2510 <trinhthuylinh03121997@gmail.com>
2023-06-16 21:25:04 -0700

    upcasepart3

--- CaseStudtyQuanLySV/Student.js
+++ CaseStudtyQuanLySV/Student.js
@@ -10,54 +10,54 @@ class Student{
         this.note = note
     }
     getNo(){
-
+return this.no
     }
     setNo(){
-
+this.no = no 
     }
     getName(){
-
+return this.name
     }
     setName(){
-
+ this.name = name
     }
     getDob(){
-
+return this.dob
     }
     setDob(){
-
+this.dob = dob
     }
     getMale(){
-
+return this.male
     }
     setMale(){
-
+this.male = male
     }
     getAddress(){
-
+return this.address
     }
     setAddress(){
-
+this.address = address
     }
     getParents(){
-
+return this.parents
     }
     setParents(){
-
+this.parents = parents 
     }
     getPhone(){
-
+return this.phone 
     }
     setPhone(){
-
+this.phone = phone 
     }
     getNote(){
-
+return this.note 
     }
     setNote(){
-
+this.note = note 
     }
     editStudent(){
-        
+        window.location.href="http://127.0.0.1:5500/CaseStudtyQuanLySV/FormEdit.html"
     }
 }
